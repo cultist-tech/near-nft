46a723d5008d659bde844c1e29bd522ca042a026
Muzikanto <schiriy_maxim@icloud.com>
2022-10-14 19:43:27 +0300

    fixes

--- scripts/nft_tokens_for_owner.sh
+++ scripts/nft_tokens_for_owner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source neardev/.env
 
-NEAR_ENV="$NEAR_ENV" near view $CONTRACT_NAME nft_tokens_for_owner "{ \"account_id\": \"$ACCOUNT_ID\", \"limit\": 5, \"from_index\": \"0\" }"
+NEAR_ENV="$NEAR_ENV" near view $CONTRACT_NAME nft_tokens_for_owner "{ \"account_id\": \"$CONTRACT_NAME\", \"limit\": 5, \"from_index\": \"1000\" }"
