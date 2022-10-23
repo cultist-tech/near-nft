669b0511e641860417aec0894295009102a19e44
Muzikanto <schiriy_maxim@icloud.com>
2022-10-23 17:30:19 +0300

    fix mint script

--- scripts/nft_mint.sh
+++ scripts/nft_mint.sh
@@ -4,14 +4,14 @@ source neardev/.env
 SALE_ID="badges"
 
 near call $CONTRACT_NAME nft_mint --accountId $CONTRACT_NAME "{
-  \"token_id\": \"_9\",
+  \"token_id\": \"14000\",
   \"token_metadata\": {
     \"title\": \"Badge 1\",
     \"description\": \"badge 1 text\",
     \"media\": \"7000.png\"
   },
   \"receiver_id\": \"muzikant.testnet\",
-  \"rarity\": \"Common\",
+  \"rarity\": 0,
   \"collection\": \"Unknown\",
   \"bind_to_owner\": false,
   \"token_type\": \"Armor\",
